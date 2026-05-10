17a8f9a820829bb035330348f46e4a2a898946f2
JamTheDev <2004jamvillarosa@gmail.com>
2026-05-11 05:31:12 +0800

    Refactor database connection setup and update dependencies in Cargo.toml

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,21 @@
+
 use std::env;
 
-#[tokio::main]
-async fn db() {
+use sqlx::postgres::{
+    PgPoolOptions,
+};
+
+
+pub async fn initialize_database() {
     match env::var("POSTGRESQL_CONNECTION") {
-        Ok(val) => println!("{}", val),
+        Ok(val) => connect(val).await,
         Err(e) => println!("Error: {}", e)
-    }
+    };
 } 
+
+// i know this is redundant but yeah ill just work on this in another time
+async fn connect(val: String) {
+    let _ = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&val).await;
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use axum:: {
 async fn main() {
     let app: Router = Router::new().route("/", get(|| async { "Hello, World! "}));
 
+    // connect to the database
+    db::initialize_database().await;
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
